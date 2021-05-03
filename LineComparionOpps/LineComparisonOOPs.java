adebd19a9c37e74f0b4b1fcac2242e3eb87d9cf6
shripadshukla <shripadshukla46@gmail.com>
2021-05-03 12:25:08 +0530

    Line Comparison Program Using OOPs added

---
+++ LineComparionOpps/LineComparisonOOPs.java
@@ -0,0 +1,22 @@
+package LineComparionOpps;
+
+public class LineComparisonOOPs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x1 = 2;
+		int x2 = 4;
+		int y1 = 6;
+		int y2 = 10;
+		
+		double length_of_line = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+		System.out.println("length of the line = " +length_of_line);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Distance();
+	}
+
+}
