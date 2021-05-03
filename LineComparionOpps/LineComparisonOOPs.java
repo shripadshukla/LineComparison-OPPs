6c62e33f9b1d8ec17917550b4b65fba005c6da07
shripadshukla <shripadshukla46@gmail.com>
2021-05-03 12:31:21 +0530

    UC2: Equal Line program added

--- LineComparionOpps/LineComparisonOOPs.java
+++ LineComparionOpps/LineComparisonOOPs.java
@@ -2,21 +2,43 @@ package LineComparionOpps;
 
 public class LineComparisonOOPs {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int x1 = 2;
-		int x2 = 4;
-		int y1 = 6;
-		int y2 = 10;
+	int x1;
+	int x2;
+	int y1;
+	int y2;
+	int x3;
+	int x4;
+	int y3;
+	int y4;
+	
+	Integer line1 = (int) Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+	
+	 Integer line2 = (int) Math.sqrt((y4 - y3) * (y4 - y3) + (x4 - x3) * (x4 - x3));
+	
+	public LineComparisonOOPs(int x1, int x2, int y1, int y2, int x3, int x4, int y3, int y4) {
 		
-		double length_of_line = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
-		System.out.println("length of the line = " +length_of_line);
+		x1 = x1;
+		x2 = x2;
+		y1 = y1;
+		y2 = y2;
 		
+		x3 = x3;
+		x4 = x4;
+		y3 = y3;
+		y4 = y4;
 	}
-
-	public static void main(String[] args) {
+	
+	
+	public static void main(String []args) {
 		
-		Distance();
+		LineComparisonOOPs UC = new LineComparisonOOPs(2, 4, 6, 10, 4, 6, 8, 12);
+		
+		
+		if(UC.line1.equals(UC.line2))
+			
+			System.out.println("Lengths of both the lines are equal");
+		
+		else
+			System.out.println("Not equal");
 	}
-
 }
